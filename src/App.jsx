b4b0beabdc8fde0d2d13e89c668adf01d0fdd669
Anirudha <beleanirudha8@gmail.com>
2024-09-25 21:03:37 +0530

    create dependencies i.e. input fields on which password will be generated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
 	const [length, setLength] = useState(8);
 	const [numberAllowed, setNumberAllowed] = useState(false);
-	const [charAllowed, setCharAllowed] = useState(false);
+	const [characterAllowed, setCharacterAllowed] = useState(false);
 	const [password, setPassword] = useState("");
 
 	const passwordGenerator = useCallback(() => {
@@ -14,14 +14,14 @@ function App() {
 
 		if (numberAllowed) str += "01234567890";
 
-		if (charAllowed) str += "!@#$%^&*()-_=+[]{}~|<>?/";
+		if (characterAllowed) str += "!@#$%^&*()-_=+[]{}~|<>?/";
 
 		for (let i = 0; i < length; i++) {
 			let charIndex = Math.floor(Math.random * str.length + 1);
 			pass = str.charAt(charIndex);
 		}
 		setPassword(pass);
-	}, [length, numberAllowed, charAllowed, setPassword]);
+	}, [length, numberAllowed, characterAllowed, setPassword]);
 
 	return (
 		<>
@@ -42,6 +42,42 @@ function App() {
 					<button className="outline-none bg-blue-500 px-4 py-3 shrink-0 hover:bg-blue-600">
 						COPY
 					</button>
+				</div>
+				<div className="flex text-md gap-x-4">
+					<div className="flex items-center gap-x-1">
+						<input
+							type="range"
+							name="range"
+							id="range"
+							min={6}
+							max={50}
+							className="cursor-pointer"
+							onChange={(e) => setLength(e.target.value)}
+						/>
+						<label htmlFor="range">Length: {length}</label>
+					</div>
+					<div className="flex items-center gap-x-1">
+						<input
+							type="checkbox"
+							name="numberInput"
+							id="numberInput"
+							defaultChecked={numberAllowed}
+							onChange={() => setNumberAllowed((prev) => !prev)}
+						/>
+						<label htmlFor="numberInput">Number</label>
+					</div>
+					<div className="flex items-center gap-x-1">
+						<input
+							type="checkbox"
+							name="characterInput"
+							id="characterInput"
+							defaultChecked={characterAllowed}
+							onChange={() =>
+								setCharacterAllowed((prev) => !prev)
+							}
+						/>
+						<label htmlFor="characterInput">Character</label>
+					</div>
 				</div>
 			</div>
 		</>
