58bfcb866a2e3291345d2b3de8a3a437276615a3
Anirudha <beleanirudha8@gmail.com>
2024-10-12 13:57:16 +0530

    add different states

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
+	const [length, setLength] = useState(8);
+	const [numberAllowed, setNumberAllowed] = useState(false);
+	const [charAllowed, setCharAllowed] = useState(false);
+	const [password, setPassword] = useState("");
+
 	return (
 		<>
 			<h1 className="text-[#eee] text-4xl">Password Generator</h1>
