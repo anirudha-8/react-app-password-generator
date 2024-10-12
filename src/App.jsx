261283425f84db820e54eb9844f946eee45914b9
Anirudha <beleanirudha8@gmail.com>
2024-10-12 14:01:56 +0530

    create "generatePassword()" method using "useCallback()" for optimization to use memoization

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,22 @@ function App() {
 	const [charAllowed, setCharAllowed] = useState(false);
 	const [password, setPassword] = useState("");
 
+	const passwordGenerator = useCallback(() => {
+		let pass = "";
+
+		let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+		if (numberAllowed) str += "01234567890";
+
+		if (charAllowed) str += "!@#$%^&*()-_=+[]{}~|<>?/";
+
+		for (let i = 0; i < length; i++) {
+			let charIndex = Math.floor(Math.random * str.length + 1);
+			pass = str.charAt(charIndex);
+		}
+		setPassword(pass);
+	}, [length, numberAllowed, charAllowed, setPassword]);
+
 	return (
 		<>
 			<h1 className="text-[#eee] text-4xl">Password Generator</h1>
